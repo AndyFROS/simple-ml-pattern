401c2a349e829c8e289517884216af381e93f749
Andrey <66909286+AndyFROS@users.noreply.github.com>
2022-12-30 20:28:14 +0300

    Update NLTK_with_Lemmatization.py

--- universe/NLTK_with_Lemmatization.py
+++ universe/NLTK_with_Lemmatization.py
@@ -1,11 +1,13 @@
 import nltk
 import string
+import pymorphy2
+import regex as re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import pymorphy2
 
 #Тестовое предложение
-sentence_example = df.iloc[0]["name"]
+sentence_example = df.iloc[0]["Вопросы"]
 
 #Создание MorphAnalyzer для использования метода лемматизации
 morph = pymorphy2.MorphAnalyzer()
@@ -14,8 +16,9 @@ stop_words = stopwords.words("russian")
 
 #Функция, которую буду использовать для обработки nltk
 def tokinize_sentence(sentense: str, remove_stop_word: bool = True):
+    #Перед использованием функции удалю все специальные символы и цифры с помощью регулярных выражений, а strip удалит лишние пробелы в начале и в конце предложения
     #Первый шаг — токенизация (у нас будет список слов)
-    tokens = word_tokenize(sentense, language="russian")
+    tokens = word_tokenize(re.compile('[^а-яА-Я ]').sub('', sentense).strip(), language="russian")
     #Второй шаг - удалить знаки препинания
     tokens = [i for i in tokens if i not in string.punctuation]
     #Затем удаляю стоп-слова, если ине это нужно.
@@ -24,7 +27,6 @@ def tokinize_sentence(sentense: str, remove_stop_word: bool = True):
     #Теперь верну слово в начальную форму
     tokens = [morph.parse(i)[0].normal_form for i in tokens]
     return tokens
-#Перед использованием функции удалю все специальные символы и цифры с помощью регулярных выражений, а strip удалит лишние пробелы в начале и в конце предложения
-sentence_example = re.compile('[^a-zA-Z ]').sub('', sentence_example).strip()
+
 #Использование функции
 tokinize_sentence(sentence_example)
